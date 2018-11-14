d9dbbddd8d5434e3a1cc70ac556cfae52eb3b5e7
drengarc <mfelek@gmail.com>
2018-11-14 10:10:25 +0300

    g

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'deneme.views.home', name='home'),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^blog/', include('blog.url1ss')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
